e17ff3cf69c3118bc8f625b7d50eac68ab398b35
RenanAlmeidaSilva <64547456+RenanAlmeidaSilva@users.noreply.github.com>
2020-10-24 18:44:09 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,20 +1,25 @@
 import os
 from flask import Flask, jsonify, request
+from math import aqrt
 
-fibo = Flask(__name__)
+app = Flask(__name__)
 
 @app.route('/')
-def fibon():
-    p = 1
-    a = 0
-    r = "0,"
+def nao_entre_em_panico():
+    proximo = 1
+    anterior = 0
+    limite = 50
+    found = 0
+    resposta = "0,"
 
-    for i in range(51):
-        tmp = p
-        p = p + a
-        a = tmp
-        r += str(p) + ","
-    return r
+    while (found < limite):
+        tmp = proximo
+        proximo = proximo + anterior
+        anterior = tmp
+        found = found + 1
+        resposta += str(proximo) + ","
+    return resposta
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5001))
-    fibo.run(host='0.0.0.0', port=port)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
+    
